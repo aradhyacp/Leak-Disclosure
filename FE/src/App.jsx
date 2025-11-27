6cfbbec9c3ad7b6f33d9150883fd0a3e29c8eed3
aradhyacp <cparadhya@proton.me>
2025-11-27 18:15:03 +0530

    feat: refactor App component and integrate ClerkProvider for authentication

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AppRoutes from './Routes/AppRoutes'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (<div className="">
-    <div className="bg-red-400 text-2xl">hey there</div>
-  </div>
+  return (
+    <AppRoutes/>
   )
 }
 
