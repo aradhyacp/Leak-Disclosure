a78950e495dff5d2dbc7d89261c007e1505d56fd
aradhyacp <cparadhya@proton.me>
2025-12-12 19:05:10 +0530

    implemented protected route and not found page

--- FE/src/Routes/AppRoutes.jsx
+++ FE/src/Routes/AppRoutes.jsx
@@ -12,21 +12,27 @@ import Dashboard from "../Pages/Dashboard";
 import Success from "../Pages/Success";
 import Cancel from "../Pages/Cancel";
 import Upgrade from "../Pages/Upgrade";
+import ProtectedRoute from "./ProtectedRoute";
+import NotFound from "../Pages/NotFound";
 
 const AppRoutes = () => {
-    const router = createBrowserRouter(createRoutesFromElements(
-        <>
-        <Route path="/" element={<Root/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/stripe/success" element={<Success/>}/>
-        <Route path="/stripe/cancel" element={<Cancel/>}/>        
-        <Route path="/upgrade" element={<Upgrade/>}/>        
-
-        </>
-    ));
-    return <RouterProvider router={router}/>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Root />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/stripe/success" element={<Success />} />
+          <Route path="/stripe/cancel" element={<Cancel />} />
+          <Route path="/upgrade" element={<Upgrade />} />
+        </Route>
+        <Route path="*" element={<NotFound/>}/>
+      </>
+    )
+  );
+  return <RouterProvider router={router} />;
 };
 
 export default AppRoutes;
