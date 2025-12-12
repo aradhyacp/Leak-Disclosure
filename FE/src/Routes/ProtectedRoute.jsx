8c48e2db104a55f7868bd3071a8285b7260173fd
aradhyacp <cparadhya@proton.me>
2025-12-12 18:59:25 +0530

    fix: format and standardize code style in ProtectedRoute component

--- FE/src/Routes/ProtectedRoute.jsx
+++ FE/src/Routes/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { useAuth } from '@clerk/clerk-react'
-import { Navigate,Outlet } from 'react-router-dom'
+import React from "react";
+import { useAuth } from "@clerk/clerk-react";
+import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
-    const{ isLoaded,isSignedIn } = useAuth()
-    if(!isLoaded)return null
-    if(isSignedIn){
-        return <Outlet/>
-    }
-  return <Navigate to="/login" replace/>
-}
+  const { isLoaded, isSignedIn } = useAuth();
+  if (!isLoaded) return null;
+  if (!isSignedIn) {
+    return <Navigate to="/login" replace />;
+  }
+  return <Outlet />;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
